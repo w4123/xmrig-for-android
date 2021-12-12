b9127536b54aaabb048dcaf932dbc4a799f6040d
Garry Lachman <garry@velotix.ai>
2021-12-12 15:00:11 +0200

    version

--- src/components/core/app-header.tsx
+++ src/components/core/app-header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Appbar } from 'react-native-paper';
+import { version  } from '../../version';
 
 export const AppHeader = ({ navigation, back }: any) => {
     return (
         <Appbar.Header>
             {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
             <Appbar.Content title="XMRig" subtitle="for Android" />
-            <Appbar.Content title="Version" subtitle="1.2.45" style={{alignItems: 'flex-end'}} />
+            <Appbar.Content title="Version" subtitle={version} style={{alignItems: 'flex-end'}} />
         </Appbar.Header>
     )
 }
