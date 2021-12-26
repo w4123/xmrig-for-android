7f9e7502f2c8548d2960e719370faba232128852
Garry Lachman <garry@lachman.co>
2021-12-26 19:31:25 +0200

    version from properties

---
+++ update_version.js
@@ -0,0 +1,16 @@
+const properties = require('java-properties');
+const fs = require('fs');
+const values = properties.of('version.properties');
+
+const package = require('./package.json')
+const newPackage = {
+    ...package,
+    version: `${values.getInt("majorVersion")}.${values.getInt("minorVersion")}.${values.getInt("patchVersion")}`
+}
+
+fs.writeFileSync('./package.json', JSON.stringify(newPackage, null, 2))
+
+const versionTS = `export const version = "${values.getInt("majorVersion")}.${values.getInt("minorVersion")}.${values.getInt("patchVersion")}"`
+
+fs.writeFileSync('./src/version.ts', versionTS)
+
