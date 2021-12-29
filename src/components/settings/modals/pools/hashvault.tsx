402344d399a0b67bfe50f4914da54abb9019fea7
Garry Lachman <garry@lachman.co>
2021-12-29 03:16:42 +0200

    Predefined Pools

---
+++ src/components/settings/modals/pools/hashvault.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { IPool } from '.';
+import { validateWalletAddress } from '../../../../core/utils';
+
+const hostname = 'pool.hashvault.pro';
+const port = 80;
+
+export const HashVault:React.FC<IPool> = ({onChange}) => {
+    const [wallet, setWallet] = React.useState<string>('');
+    const [worker, setWorker] = React.useState<string>('');
+
+    React.useEffect(() => {
+        onChange({
+            hostname: hostname,
+            port: port,
+            username: `${wallet}`,
+            password: `${worker}`,
+        })
+    }, [wallet, worker]);
+
+    return (
+        <>
+            <TextInput
+                style={styles.input}
+                label="Wallet Address"
+                dense
+                value={wallet}
+                onChangeText={setWallet}
+                children={undefined}
+                autoComplete={undefined}
+                error={!validateWalletAddress(wallet)}            
+            />
+            <TextInput
+                style={styles.input}
+                label="Worker Name (Optional)"
+                placeholder='Worker1'
+                dense
+                value={worker}
+                onChangeText={setWorker}
+                children={undefined}
+                autoComplete={undefined}                
+            />
+        </>
+    )
+};
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 20
+    }
+});
---
+++ src/components/settings/modals/pools/xmrpool-eu.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { IPool } from '.';
+import { validateWalletAddress } from '../../../../core/utils';
+
+const hostname = 'xmrpool.eu';
+const port = 5555;
+
+export const XMRPoolEU:React.FC<IPool> = ({onChange}) => {
+    const [wallet, setWallet] = React.useState<string>('');
+    const [worker, setWorker] = React.useState<string>('');
+    const [difficulty, setDifficulty] = React.useState<string>('');
+
+    React.useEffect(() => {
+        onChange({
+            hostname: hostname,
+            port: port,
+            username: `${wallet}${difficulty ? '.' : ''}${difficulty}${worker ? '+' : ''}${worker}`,
+            password: `x`,
+        })
+    }, [wallet, worker, difficulty]);
+
+    return (
+        <>
+            <TextInput
+                style={styles.input}
+                label="Wallet Address"
+                dense
+                value={wallet}
+                onChangeText={setWallet}
+                children={undefined}
+                autoComplete={undefined}
+                error={!validateWalletAddress(wallet)}            
+            />
+            <TextInput
+                style={styles.input}
+                label="Worker Name (Optional)"
+                placeholder='Worker1'
+                dense
+                value={worker}
+                onChangeText={setWorker}
+                children={undefined}
+                autoComplete={undefined}                
+            />
+            <TextInput
+                style={styles.input}
+                label="Fixed Difficulty (Optional)"
+                keyboardType='numeric'
+                placeholder='128000'
+                dense
+                value={difficulty}
+                onChangeText={setDifficulty}
+                children={undefined}
+                autoComplete={undefined}                
+            />
+        </>
+    )
+};
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 20
+    }
+});
