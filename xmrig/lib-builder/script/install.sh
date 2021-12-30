90573423ca2ce4c8c1dfaacd206cc05d44f316f0
Garry Lachman <garry@lachman.co>
2021-12-30 21:44:48 +0200

    XMRig MO build scripts

--- xmrig/lib-builder/script/install.sh
+++ xmrig/lib-builder/script/install.sh
@@ -27,9 +27,11 @@ for arch in ${archs[@]}; do
 
 	ROOT_DIR=`pwd`/../../
 	XMRIG_DIR=`pwd`/build/src/xmrig/build/$xarch
+    XMRIG_MO_DIR=`pwd`/build/src/xmrig-mo/build/$xarch
 
 	rm -Rf $ROOT_DIR/android/app/src/main/jniLibs/$xarch/*
 	cp $XMRIG_DIR/xmrig $ROOT_DIR/android/app/src/main/jniLibs/$xarch/libxmrig.so
+    cp $XMRIG_MO_DIR/xmrig $ROOT_DIR/android/app/src/main/jniLibs/$xarch/libxmrig-mo.so
 
 done
 exit 0
