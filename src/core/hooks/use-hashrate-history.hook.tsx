110ab246b60692b1917edef00c18fc54294ee226
Garry Lachman <garry@lachman.co>
2022-02-05 23:37:24 +0200

    Change history num of records

--- src/core/hooks/use-hashrate-history.hook.tsx
+++ src/core/hooks/use-hashrate-history.hook.tsx
@@ -3,7 +3,7 @@ import { useSimpleMovingAverage } from './use-moving-average.hook';
 
 export const useHashrateHistory = (initial:number[]) => {
   const [history, setHistory] = React.useState<number[]>(initial);
-  const add = React.useCallback((value:number) => setHistory((h) => [...h, value].slice(-200)), []);
+  const add = React.useCallback((value:number) => setHistory((h) => [...h, value].slice(-60)), []);
   const reset = () => setHistory(initial);
   const sma = useSimpleMovingAverage(history, 6);
   return {
